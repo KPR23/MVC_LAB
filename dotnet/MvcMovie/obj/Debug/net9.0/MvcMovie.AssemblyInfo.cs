835322c19bd90c5f9605271bcb8e0183c441ba4e
KPR23 <zabluka_aehit@students.vizja.pl>
2025-03-20 09:15:20 +0100

    Chapter 3 completed

--- dotnet/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ dotnet/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56db484d7dfd9cd1a27616a3cf0e8a07f97158f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8b1bf2ed127009037f64e2ea7f7d5ee156d6ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
