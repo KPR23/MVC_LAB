b7eaa9ca30b02c5a87a58af13ad1dc4ea74737e7
KPR23 <zabluka_aehit@students.vizja.pl>
2025-04-30 12:11:16 +0200

    Add event-related API and page components; update database schema for events

--- projekt/src/server/db/schema.ts
+++ projekt/src/server/db/schema.ts
@@ -1,5 +1,20 @@
-import { pgTable, uuid } from 'drizzle-orm/pg-core';
+import {
+  date,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core';
 
-export const testTable = pgTable('test_table', {
+export const events = pgTable('events', {
   id: uuid('id').primaryKey().defaultRandom(),
+  title: text('title').notNull(),
+  description: text('description').notNull(),
+  location: text('location').notNull(),
+  imageUrl: text('image_url').notNull(),
+  date: date('date').notNull(),
+  capacity: integer('capacity').notNull(),
+  price: integer('price').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
 });
